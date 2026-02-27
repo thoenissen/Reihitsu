4d1ff55141dac18a058fb9de9fa1dfa6989e1f5a
thoenissen <26723285+thoenissen@users.noreply.github.com>
2026-02-27 22:47:12 +0100

    Testing of xml documentation renaming of RH0208

--- Reihitsu.Analyzer/Reihitsu.Analyzer.Test/Naming/Resources/RH0208.ResultData.cs
+++ Reihitsu.Analyzer/Reihitsu.Analyzer.Test/Naming/Resources/RH0208.ResultData.cs
@@ -5,5 +5,7 @@ namespace Reihitsu.Analyzer.Test.Naming.Resources
     /// <summary>
     /// Test delegate
     /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="e">Event args</param>
     public delegate void EventHandler(object sender, EventArgs e);
 }
