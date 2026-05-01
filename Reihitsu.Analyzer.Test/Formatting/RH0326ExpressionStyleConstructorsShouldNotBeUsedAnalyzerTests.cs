fa401ff95e7eb13f6a6809529b7741689d328254
thoenissen <26723285+thoenissen@users.noreply.github.com>
2026-05-01 15:14:10 +0200

    Add RH0326 code fix

--- Reihitsu.Analyzer.Test/Formatting/RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzerTests.cs
+++ Reihitsu.Analyzer.Test/Formatting/RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,27 +12,54 @@ namespace Reihitsu.Analyzer.Test.Formatting;
 /// Test methods for <see cref="RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzer"/>
 /// </summary>
 [TestClass]
-public class RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzerTests : AnalyzerTestsBase<RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzer>
+public class RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzerTests : AnalyzerTestsBase<RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzer, RH0326ExpressionStyleConstructorsShouldNotBeUsedCodeFixProvider>
 {
     #region Members
 
     /// <summary>
-    /// Verifying that expression-bodied constructors are detected
+    /// Verifying that an expression-bodied constructor is detected and fixed
     /// </summary>
     /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
     [TestMethod]
-    public async Task VerifyExpressionBodiedConstructorsAreDetected()
+    public async Task VerifyExpressionBodiedConstructorsAreDetectedAndFixed()
+    {
+        const string testData = """
+                                internal class RH0326
+                                {
+                                    {|#0:public RH0326() => System.Console.WriteLine();|}
+                                }
+                                """;
+        const string fixedData = """
+                                 internal class RH0326
+                                 {
+                                     public RH0326()
+                                     {
+                                         System.Console.WriteLine();
+                                     }
+                                 }
+                                 """;
+
+        await Verify(testData,
+                     fixedData,
+                     Diagnostics(RH0326ExpressionStyleConstructorsShouldNotBeUsedAnalyzer.DiagnosticId, AnalyzerResources.RH0326MessageFormat));
+    }
+
+    /// <summary>
+    /// Verifying that multiple expression-bodied constructors are detected
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+    [TestMethod]
+    public async Task VerifyMultipleExpressionBodiedConstructorsAreDetected()
     {
         const string testData = """
                                 internal class RH0326
                                 {
                                     {|#0:public RH0326() => System.Console.WriteLine();|}
                                     {|#1:public RH0326(int i) => System.Console.WriteLine(i);|}
-                                    
                                 }
 
                                 internal class RH0326Expression
-                                {   
+                                {
                                     public RH0326Expression()
                                     {
                                         System.Console.WriteLine();
