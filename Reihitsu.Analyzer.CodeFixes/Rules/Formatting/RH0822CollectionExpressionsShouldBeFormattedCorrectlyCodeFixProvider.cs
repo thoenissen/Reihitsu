b53f4541d2b96f28409ed3e8b46438a583c30858
Seth <26723285+thoenissen@users.noreply.github.com>
2026-05-21 10:30:02 +0200

    Add RH0822 collection expression formatting rule (#179)

---
+++ Reihitsu.Analyzer.CodeFixes/Rules/Formatting/RH0822CollectionExpressionsShouldBeFormattedCorrectlyCodeFixProvider.cs
@@ -0,0 +1,99 @@
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Reihitsu.Formatter;
+
+namespace Reihitsu.Analyzer.Rules.Formatting;
+
+/// <summary>
+/// Providing fixes for <see cref="RH0822CollectionExpressionsShouldBeFormattedCorrectlyAnalyzer"/>
+/// </summary>
+[Shared]
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RH0822CollectionExpressionsShouldBeFormattedCorrectlyCodeFixProvider))]
+public class RH0822CollectionExpressionsShouldBeFormattedCorrectlyCodeFixProvider : CodeFixProvider
+{
+    #region Methods
+
+    /// <summary>
+    /// Applying code fix
+    /// </summary>
+    /// <param name="document">Document</param>
+    /// <param name="formatTarget">Node that should be formatted</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+    private static async Task<Document> ApplyCodeFixAsync(Document document, SyntaxNode formatTarget, CancellationToken cancellationToken)
+    {
+        return await ReihitsuFormatter.FormatNodeInDocumentAsync(document, formatTarget, cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Try to get the formatting scope that should be formatted
+    /// </summary>
+    /// <param name="root">Root</param>
+    /// <param name="diagnostic">Diagnostic</param>
+    /// <returns>The formatting scope</returns>
+    private static SyntaxNode TryGetFormatTarget(SyntaxNode root, Diagnostic diagnostic)
+    {
+        var collectionExpression = root.FindToken(diagnostic.Location.SourceSpan.Start).Parent?.AncestorsAndSelf()
+                                       .OfType<CollectionExpressionSyntax>()
+                                       .FirstOrDefault();
+
+        if (collectionExpression == null)
+        {
+            return null;
+        }
+
+        return collectionExpression.AncestorsAndSelf()
+                                   .FirstOrDefault(node => node is StatementSyntax
+                                                                or EqualsValueClauseSyntax
+                                                                or ArrowExpressionClauseSyntax
+                                                                or LambdaExpressionSyntax
+                                                                or SwitchExpressionArmSyntax
+                                                                or ArgumentSyntax)
+                   ?? collectionExpression;
+    }
+
+    #endregion // Methods
+
+    #region CodeFixProvider
+
+    /// <inheritdoc/>
+    public sealed override ImmutableArray<string> FixableDiagnosticIds => [RH0822CollectionExpressionsShouldBeFormattedCorrectlyAnalyzer.DiagnosticId];
+
+    /// <inheritdoc/>
+    public sealed override FixAllProvider GetFixAllProvider()
+    {
+        return WellKnownFixAllProviders.BatchFixer;
+    }
+
+    /// <inheritdoc/>
+    public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+        if (root != null)
+        {
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                var formatTarget = TryGetFormatTarget(root, diagnostic);
+
+                if (formatTarget != null)
+                {
+                    context.RegisterCodeFix(CodeAction.Create(CodeFixResources.RH0822Title,
+                                                              c => ApplyCodeFixAsync(context.Document, formatTarget, c),
+                                                              nameof(RH0822CollectionExpressionsShouldBeFormattedCorrectlyCodeFixProvider)),
+                                            diagnostic);
+                }
+            }
+        }
+    }
+
+    #endregion // CodeFixProvider
+}
