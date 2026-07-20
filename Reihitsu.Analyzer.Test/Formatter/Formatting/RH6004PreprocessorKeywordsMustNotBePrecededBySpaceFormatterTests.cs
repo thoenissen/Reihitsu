bc1624af74d2f8891a2fd643cf378b68e98a2312
thoenissen <26723285+thoenissen@users.noreply.github.com>
2026-07-20 18:56:00 +0200

    Fix RH6004 non-formattable line exclusions (#443) [skip ci]

--- Reihitsu.Analyzer.Test/Formatter/Formatting/RH6004PreprocessorKeywordsMustNotBePrecededBySpaceFormatterTests.cs
+++ Reihitsu.Analyzer.Test/Formatter/Formatting/RH6004PreprocessorKeywordsMustNotBePrecededBySpaceFormatterTests.cs
@@ -36,5 +36,38 @@ public class RH6004PreprocessorKeywordsMustNotBePrecededBySpaceFormatterTests : 
                                  Diagnostics(RH6004PreprocessorKeywordsMustNotBePrecededBySpaceAnalyzer.DiagnosticId, AnalyzerResources.RH6004MessageFormat));
     }
 
+    /// <summary>
+    /// Verifies that the formatter moves an indented member-level pragma to column zero
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+    [TestMethod]
+    public async Task VerifyFormatterFixesMemberLevelPragma()
+    {
+        const string testData = """
+                                internal class TestClass
+                                {
+                                    private void Method()
+                                    {
+                                {|#0:        |}#pragma warning disable CS0168
+                                        int value = 0;
+                                    }
+                                }
+                                """;
+        const string fixedData = """
+                                 internal class TestClass
+                                 {
+                                     private void Method()
+                                     {
+                                 #pragma warning disable CS0168
+                                         int value = 0;
+                                     }
+                                 }
+                                 """;
+
+        await VerifyFormatterFix(testData,
+                                 fixedData,
+                                 Diagnostics(RH6004PreprocessorKeywordsMustNotBePrecededBySpaceAnalyzer.DiagnosticId, AnalyzerResources.RH6004MessageFormat));
+    }
+
     #endregion // Tests
 }
